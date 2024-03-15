20084617c568b7a2f59fe36174f97a6e12783e04
Sadman Mehrab <sadmannaqueeb@gmail.com>
2024-03-15 19:45:20 +0600

    setup database

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FacultyModule } from './faculty/faculty.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [FacultyModule],
+  imports: [FacultyModule, TypeOrmModule.forRoot({
+    type:'postgres',
+    host:'localhost',
+    port:5432,
+    username:'postgres',
+    password:'POKPOK123',
+    database:'APWT',
+    autoLoadEntities: true,
+    synchronize: true,
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
